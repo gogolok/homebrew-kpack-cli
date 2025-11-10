f5cc279902f4deb722b84578a02d1f9c636b01de
Robert Gogolok <robert.gogolok@stackit.cloud>
2025-11-10 18:20:04 +0100

    Update Kpack CLI to 0.13.1
    Signed-off-by: Robert Gogolok <robert.gogolok@stackit.cloud>

--- kp.rb
+++ kp.rb
@@ -1,23 +1,23 @@
   class Kp < Formula
   desc "A command line interface for interacting with kpack."
-  homepage "https://github.com/vmware-tanzu/kpack-cli"
+  homepage "https://github.com/buildpacks-community/kpack-cli"
   license "Apache-2.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.13.0/kp-darwin-amd64-0.13.0"
-      sha256 "fd621c0bba78abece675fbe91343b63c8b3db6f40b27bb859c83ebdfcb979723"
+      url "https://github.com/buildpacks-community/kpack-cli/releases/download/v0.13.1/kp-darwin-amd64-0.13.1"
+      sha256 "87d14771a07319c48a73c022a12e385f59ba515e02c7d859ef0c71fc002ba52e"
     elsif Hardware::CPU.arm?
-      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.13.0/kp-darwin-arm64-0.13.0"
-      sha256 "bd243696d54154bfd93b1708d90acac9f68c48b09658f58ff455d4fdb4a7c56f"
+      url "https://github.com/buildpacks-community/kpack-cli/releases/download/v0.13.1/kp-darwin-arm64-0.13.1"
+      sha256 "ed352dafffcbf60c1001b65585fe589b987b9146c1f93bafe12d2aaae20e8470"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.13.0/kp-linux-amd64-0.13.0"
-      sha256 "52f0c927a1350f4f1bb281575ec246f406fb96aa69dc974ed10a2fe52c538158"
+      url "https://github.com/buildpacks-community/kpack-cli/releases/download/v0.13.1/kp-linux-amd64-0.13.1"
+      sha256 "c5b8a675f58489e77bb75c340c425568930ad694906dc3657abac566b06b0011"
     elsif Hardware::CPU.arm?
-      url "https://github.com/vmware-tanzu/kpack-cli/releases/download/v0.13.0/kp-linux-arm64-0.13.0"
-      sha256 "a4d5cd0ec8187945c098b450a617ac86bb6aa062f517ebb6c1b66f1d8d7e9bc2"
+      url "https://github.com/buildpacks-commujnity/kpack-cli/releases/download/v0.13.1/kp-linux-arm64-0.13.1"
+      sha256 "21ebaa7aadbde560e6b63314306e9c90a63bd1eca9d89da1c86491aecb0a0999"
     end
   end
 
